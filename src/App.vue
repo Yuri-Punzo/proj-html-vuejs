d12be60ae46c598b39943f7c506d0f5d0e5e04b5
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-21 18:23:15 +0100

    added font-family Fira Sans + wip

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,27 @@ export default {
 </script>
 
 <template>
+  <div id="site_header">
+    <!-- navbar dinamica -->
+  </div>
+  <!-- /#site_header -->
+  <div id="site_main">
+    <!-- forum section dinamico -->
 
+  </div>
+  <!-- /#site_main -->
+  <div id="site_footer">
+    <!-- site footer dinamico -->
+  </div>
+  <!-- /#site_footer -->
 </template>
 
 <style lang="scss" scoped>
+h1 {
+  font-size: 88px;
+}
 
+p {
+  font-size: 15px;
+}
 </style>
