341ad3940efec62292f410e282d79ecee05f65b9
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-22 12:02:06 +0100

    info section

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -7,7 +7,7 @@ export default {
 <template>
     <div id="site_main">
         <div class="my_jumbotron d-flex align-items-center">
-            .<div class="container">
+            <div class="container">
                 <h1>Tech Forum!</h1>
                 <p>Discuss all the latest technology news and trends</p>
                 <span class="call_to_action">
@@ -16,6 +16,21 @@ export default {
                 </span>
             </div>
         </div>
+        <!-- /jumbotron -->
+        <div class="info d-flex justify-content-center">
+            <div class="my_container text-center">
+                <h5>
+                    AVADA FORUM IS THE PLACE TO BE
+                </h5>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem error dolorem, molestiae,
+                    maiores
+                    sequi adipisci voluptas assumenda cum laboriosam, perferendis dolor veniam! Omnis fugiat distinctio
+                    molestiae illo saepe itaque. Reiciendis.
+                </p>
+            </div>
+        </div>
+        <!-- /info -->
         <!-- forum section dinamico -->
 
     </div>
@@ -60,5 +75,27 @@ export default {
 
 .chevron_right {
     font-size: 13px;
+}
+
+.info {
+    background-color: #f2f2f2;
+}
+
+.my_container {
+    width: 45%;
+    padding: 70px 0;
+
+    h5 {
+        font-weight: bold;
+        font-size: 12px;
+        color: #525252;
+        padding-bottom: 12px;
+        letter-spacing: 4px;
+    }
+
+    p {
+        font-size: 18px;
+        color: #898989;
+    }
 }
 </style>
