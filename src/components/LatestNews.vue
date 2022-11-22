fd64252045f766e29770506239dd1188936cb4d5
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-22 15:08:44 +0100

    latest news progress

--- src/components/LatestNews.vue
+++ src/components/LatestNews.vue
@@ -5,7 +5,7 @@ export default {
         return {
             articlesList: [
                 {
-                    image: "post_feat_img_25-320x202.jpg",
+                    image: "post_feat_img_25-700x441.jpg",
                     title: "Article 1 - Blah blah blah",
                     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sapiente mollitia ut dignissimos dicta, earum saepe animi deleniti aspernatur incidunt?",
                     date: "October 11th, 2022",
@@ -13,7 +13,7 @@ export default {
                     featured: false
                 },
                 {
-                    image: "post_feat_img_24-320x202.jpg",
+                    image: "post_feat_img_24-700x441.jpg",
                     title: "Article 2 - Blah blah blah",
                     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sapiente mollitia ut dignissimos dicta, earum saepe animi deleniti aspernatur incidunt?",
                     date: "October 11th, 2022",
@@ -21,7 +21,7 @@ export default {
                     featured: false
                 },
                 {
-                    image: "post_feat_img_23-320x202.jpg",
+                    image: "post_feat_img_23-700x441.jpg",
                     title: "Article 3 - Blah blah blah",
                     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sapiente mollitia ut dignissimos dicta, earum saepe animi deleniti aspernatur incidunt?",
                     date: "October 11th, 2022",
@@ -29,7 +29,7 @@ export default {
                     featured: false
                 },
                 {
-                    image: "post_feat_img_22-320x202.jpg",
+                    image: "post_feat_img_22-700x441.jpg",
                     title: "Article 4 - Blah blah blah",
                     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sapiente mollitia ut dignissimos dicta, earum saepe animi deleniti aspernatur incidunt?",
                     date: "October 11th, 2022",
@@ -37,7 +37,7 @@ export default {
                     featured: false
                 },
                 {
-                    image: "post_feat_img_21-320x202.jpg",
+                    image: "post_feat_img_21-700x441.jpg",
                     title: "Article 5 - Blah blah blah",
                     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sapiente mollitia ut dignissimos dicta, earum saepe animi deleniti aspernatur incidunt?",
                     date: "October 11th, 2022",
@@ -45,7 +45,7 @@ export default {
                     featured: false
                 },
                 {
-                    image: "post_feat_img_20-320x202.jpg",
+                    image: "post_feat_img_20-700x441.jpg",
                     title: "Article 6 - Blah blah blah",
                     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nam sapiente mollitia ut dignissimos dicta, earum saepe animi deleniti aspernatur incidunt?",
                     date: "October 11th, 2022",
@@ -69,28 +69,97 @@ export default {
 
 <template>
     <section class="latest_news">
-        <div v-for="article in articlesList.slice(0, 3)">
-            <img v-show="article.featured === false" :src="`src/assets/img/${article.image}`" alt="">
-            <h5>{{ article.title }}</h5>
-            <p>{{ article.date }} | {{ article.comments }}</p>
-            <p>{{ article.text }}</p>
-        </div>
-
-        <div v-for="article in articlesList">
-            <img v-show="article.featured === true" :src="`src/assets/img/${article.image}`" alt="">
-            <h5 v-show="article.featured === true">{{ article.title }}</h5>
-            <p v-show="article.featured === true">{{ article.date }} | {{ article.comments }}</p>
-            <p v-show="article.featured === true">{{ article.text }}</p>
-        </div>
-        <div v-for="article in articlesList.slice(3)">
-            <img v-show="article.featured === false" :src="`src/assets/img/${article.image}`" alt="">
-            <h5>{{ article.title }}</h5>
-            <p>{{ article.date }} | {{ article.comments }}</p>
-            <p>{{ article.text }}</p>
+        <div class="container">
+            <div class="latest_news_header text-center">
+                <h5>
+                    PHASELLUS EGET METUS
+                </h5>
+                <h3>
+                    All the latest news
+                </h3>
+                <div class="orange_br"></div>
+            </div>
+            <!-- /latest_news_header -->
+            <div class="row">
+                <div class="col all_articles" v-for="article in articlesList.slice(0, 3)"
+                    v-show="article.featured === false">
+                    <img :src="`src/assets/img/${article.image}`" alt="">
+                    <h5>{{ article.title }}</h5>
+                    <p>{{ article.date }} | {{ article.comments }}</p>
+                    <p>{{ article.text }}</p>
+                </div>
+            </div>
+            <!-- /first 3 articles -->
+            <div class="featured_article position-relative" v-for="article in articlesList">
+                <img class="featured_image" v-show="article.featured === true" :src="`src/assets/img/${article.image}`"
+                    alt="">
+                <div class="featured_info">
+                    <h5 v-show="article.featured === true">{{ article.title }}</h5>
+                    <p v-show="article.featured === true">{{ article.date }} | {{ article.comments }}</p>
+                    <p v-show="article.featured === true">{{ article.text }}</p>
+                </div>
+            </div>
+            <!-- /featured article -->
+            <div class="row">
+                <div class="col all_articles" v-for="article in articlesList.slice(3)"
+                    v-show="article.featured === false">
+                    <img :src="`src/assets/img/${article.image}`" alt="">
+                    <h5>{{ article.title }}</h5>
+                    <p>{{ article.date }} | {{ article.comments }}</p>
+                    <p>{{ article.text }}</p>
+                </div>
+            </div>
+            <!-- /last 3 articles -->
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
+.latest_news_header {
+    padding-top: 70px;
 
+    h5 {
+        font-weight: bold;
+        font-size: 12px;
+        color: #525252;
+        padding-bottom: 12px;
+        letter-spacing: 4px;
+    }
+
+    h3 {
+        font-size: 35px;
+        font-weight: bold;
+        color: black;
+        padding-bottom: 20px;
+    }
+
+    .orange_br {
+        width: 52px;
+        margin: auto;
+        border-bottom: 2px solid #ff8d61;
+    }
+}
+
+.all_articles {
+    margin-top: 50px;
+
+    img {
+        max-width: 100%;
+    }
+}
+
+.featured_article {
+    margin-top: 50px;
+    margin-bottom: 40px;
+
+    .featured_image {
+        max-width: 100%;
+    }
+
+    .featured_info {
+        position: absolute;
+        top: 1px;
+        left: 1px;
+    }
+}
 </style>
