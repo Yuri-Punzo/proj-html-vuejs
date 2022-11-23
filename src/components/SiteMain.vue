b6b81e94bdf480317145fa987b51a9b45b7680cc
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-23 10:53:33 +0100

    added newsletter section

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -77,13 +77,18 @@ export default {
                     <font-awesome-icon class="chevron_right" icon="fa-solid fa-chevron-right" />
                 </span>
             </div>
-
         </section>
         <!-- /join our community -->
         <section class="newsletter text-center">
             <h4>
                 Subscribe to our Newsletter
             </h4>
+            <div class="my_form d-flex justify-content-center align-items-center">
+                <input placeholder="Insert your email*" type="email" id="email" name="email">
+                <span class="newsletter_sub">
+                    SEND
+                </span>
+            </div>
         </section>
         <!-- /newsletter subscribe -->
     </div>
@@ -221,11 +226,41 @@ export default {
 
 .newsletter {
     background-color: #f2f2f2;
-    height: 300px;
 
     h4 {
-        padding-top: 70px;
+        padding-top: 60px;
         font-size: 24px;
+    }
+
+    .my_form {
+        padding-top: 20px;
+        padding-bottom: 70px;
+    }
+
+    #email {
+        width: 360px;
+        color: black;
+        font-size: 12px;
+        padding: 8px;
+        border: 1px #e2e2e2;
+        margin-right: 24px;
+    }
+
+    .newsletter_sub {
+        padding: 6px 64px;
+        letter-spacing: 1px;
+        background-color: black;
+        color: white;
+        border-radius: 2px;
+        font-weight: bold;
+        transition: 0.3s;
+
+        &:hover {
+            cursor: pointer;
+            background-color: white;
+            color: black;
+            box-shadow: 0 0 0 3px white, inset 0 0 0 2px black;
+        }
     }
 }
 </style>
